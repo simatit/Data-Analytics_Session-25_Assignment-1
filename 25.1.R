ae585d5429dd1c139d8cc09fec7c7842b272d442
SIMATIT MUNI <simatitcse@gmail.com>
2018-12-17 11:10:52 +0530

    Data-Analytics_Session-25_Assignment-1

---
+++ 25.1.R
@@ -0,0 +1,10 @@
+# Problem
+# Problem Statement
+# Set up/Install the database sakila. The database will be used for next MySQL Assignments as
+# well. Refer the Links given above for help in setting up the database.
+
+#Answers
+
+# I have gone through all the links.
+# Download the sakila database on my end,
+# and moreover there is nothing anything to write about how I download the database because it's a basic thing to do.
